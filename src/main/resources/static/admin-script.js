006f5d3644c3617fecc8ca1f752a230835bd3dff
Jason Lok <71880059+jxlok@users.noreply.github.com>
2023-02-15 09:31:12 +0000

    Implemented edit functionalities for items for sale

--- src/main/resources/static/admin-script.js
+++ src/main/resources/static/admin-script.js
@@ -1,4 +1,19 @@
 
+window.onload = function(){
+    activateEditButtons()
+}
+
+let listener = function editMode(){
+    editItem(this.parentNode);
+}
+
+function activateEditButtons(){
+    let items = document.getElementsByClassName("item-edit-icon")
+    for(var i=0;i<items.length;i++){
+        items[i].addEventListener("click", listener);
+    }
+}
+
 function analytics_tab(){
     let analytics = document.getElementById('data-analytics');
     let order_history = document.getElementById("order-history")
@@ -20,41 +35,28 @@ function items_tab(){
 }
 
 function addItem(){
-    // let items = document.getElementById("items")
-    //
-    // items.innerHTML =
-    //     "<form id='addItem-form'>" +
-    //     "   <div class=\"item\">" +
-    //     "       <span class=\"item-name\"><input type='text' placeholder='Enter item title'></span>\n" +
-    //     "       <span class=\"item-desc\"><textarea name='change-description' placeholder='Enter Description' cols='40' rows='6'></textarea></span>\n" +
-    //     "       <span class=\"item-price\">$<input type='text'></span>\n" +
-    //     "       <span class=\"item-quantity\"><input type='text'></span>\n" +
-    //     "       <span id='confirm-change' class=\"item-edit-icon\">/tick</span>\n" +
-    //     "       <span id='delete-change' class=\"item-delete-icon\">X</span>" +
-    //     "   </div>" +
-    //     "</form>"
-    //     + items.innerHTML;
-
     let form = document.getElementById("addItem-form");
     form.removeAttribute("hidden")
 
     let addItemButton = document.getElementById("addItem");
     addItemButton.removeAttribute("onclick");
+
+    deactivateEditButtons()
 }
 
 function addNewItem() {
 
     let form = document.getElementById("addItem-form");
     let items = document.getElementById("items");
-    let desc = document.getElementById("desc-change");
+    let desc = document.getElementById("desc-new");
     items.innerHTML =
         "   <div class=\"item\">" +
         "       <span class=\"item-name\"><strong>"+form.name.value+"</strong></span>\n" +
         "       <span class=\"item-desc\"><p><strong>Description:</strong> "+desc.value+"</p></span>\n" +
         "       <span class=\"item-price\">$"+ form.price.value +"</span>\n" +
         "       <span class=\"item-quantity\">"+ form.quant.value +"</span>\n" +
-        "       <span class=\"item-edit-icon\">!</span>\n" +
-        "       <span class=\"item-delete-icon\">hide</span>" +
+        "       <span class=\"item-edit-icon\"><p>edit</p></span>\n" +
+        "       <span class=\"item-hide-icon\"><p>hide</p></span>" +
         "   </div>"
         + items.innerHTML;
 
@@ -67,7 +69,83 @@ function resetAddItem(){
     addform.reset();
     addform.setAttribute("hidden", "hidden");
 
+    activateAddButton()
+    activateEditButtons()
+
+}
+
+function activateAddButton(){
     let button = document.getElementById("addItem");
     button.setAttribute("onclick", "addItem()")
 }
 
+function deactivateEditButtons(){
+    let items = document.getElementsByClassName("item-edit-icon")
+    for(var i=0;i<items.length;i++){
+        items[i].removeEventListener("click", listener);
+    }
+}
+
+function editItem(item){
+
+    item.setAttribute("id", "editing")
+    let inputs = item.getElementsByTagName("span");
+
+    let name = inputs[0].innerText;
+    let desc = inputs[1].innerText.slice(13,);
+    let price = inputs[2].innerText.slice(1,);
+    let quantity = inputs[3].innerText;
+
+    item.innerHTML =
+        "<form id='editItem-form' onsubmit='changeItemDetails(this);return false'>" +
+        "    <span class=\"item-name\"><input type='text' name='name' placeholder='Enter item title' value='"+name+"' required></span>\n" +
+        "    <span class=\"item-desc\"><textarea id='desc-changing' name='change-description' placeholder='Enter Description' cols='40' rows='6' required>"+desc+"</textarea></span>\n" +
+        "    <span class=\"item-price\">$<input type='text' name='price' value='"+price+"' required></span>\n" +
+        "    <span class=\"item-quantity\"><input type='text' name='quant' value='"+quantity+"' required></span>\n" +
+        "    <span id='confirm-change' class=\"item-change-icon\"><button type=\"submit\">&#10003</button></span>\n" +
+        "    <span id='delete-change' class=\"item-delete-icon\" onclick=\"undoChange('"+name+"','"+desc+"',"+price+","+quantity+")\">cancel</span>" +
+        "</form>"
+
+
+
+    let addItemButton = document.getElementById("addItem");
+    addItemButton.removeAttribute("onclick");
+
+    deactivateEditButtons()
+
+}
+
+
+function changeItemDetails(form){
+
+    let item = form.parentNode;
+    let desc = document.getElementById("desc-changing");
+    item.innerHTML =
+        "       <span class=\"item-name\"><strong>"+form.name.value+"</strong></span>\n" +
+        "       <span class=\"item-desc\"><p><strong>Description:</strong> "+desc.value+"</p></span>\n" +
+        "       <span class=\"item-price\">$"+ form.price.value +"</span>\n" +
+        "       <span class=\"item-quantity\">"+ form.quant.value +"</span>\n" +
+        "       <span class=\"item-edit-icon\"><p>edit</p></span>\n" +
+        "       <span class=\"item-hide-icon\"><p>hide</p></span>"
+
+    activateEditButtons()
+    activateAddButton()
+}
+
+function undoChange(name, desc, price, quantity){
+
+    let item = document.getElementById("editing")
+    item.innerHTML =
+        "       <span class=\"item-name\"><strong>"+name+"</strong></span>\n" +
+        "       <span class=\"item-desc\"><p><strong>Description:</strong> "+desc+"</p></span>\n" +
+        "       <span class=\"item-price\">$"+ price +"</span>\n" +
+        "       <span class=\"item-quantity\">"+ quantity +"</span>\n" +
+        "       <span class=\"item-edit-icon\"><p>edit</p></span>\n" +
+        "       <span class=\"item-hide-icon\"><p>hide</p></span>"
+
+    activateEditButtons()
+    activateAddButton()
+
+    item.removeAttribute("id")
+
+}
--- target/classes/static/admin-script.js
+++ target/classes/static/admin-script.js
@@ -1,4 +1,13 @@
 
+window.onload = function(){
+        let items = document.getElementsByClassName("item-edit-icon")
+        for(var i=0;i<items.length;i++){
+            items[i].addEventListener("click", function(){
+                editItem(this.parentNode);
+            })
+        }
+}
+
 function analytics_tab(){
     let analytics = document.getElementById('data-analytics');
     let order_history = document.getElementById("order-history")
@@ -17,5 +26,87 @@ function items_tab(){
     items.removeAttribute("hidden");
     analytics.setAttribute("hidden", "hidden");
     order_history.setAttribute("hidden", "hidden");
-
 }
+
+function addItem(){
+    // let items = document.getElementById("items")
+    //
+    // items.innerHTML =
+    //     "<form id='addItem-form'>" +
+    //     "   <div class=\"item\">" +
+    //     "       <span class=\"item-name\"><input type='text' placeholder='Enter item title'></span>\n" +
+    //     "       <span class=\"item-desc\"><textarea name='change-description' placeholder='Enter Description' cols='40' rows='6'></textarea></span>\n" +
+    //     "       <span class=\"item-price\">$<input type='text'></span>\n" +
+    //     "       <span class=\"item-quantity\"><input type='text'></span>\n" +
+    //     "       <span id='confirm-change' class=\"item-edit-icon\">/tick</span>\n" +
+    //     "       <span id='delete-change' class=\"item-delete-icon\">X</span>" +
+    //     "   </div>" +
+    //     "</form>"
+    //     + items.innerHTML;
+
+    let form = document.getElementById("addItem-form");
+    form.removeAttribute("hidden")
+
+    let addItemButton = document.getElementById("addItem");
+    addItemButton.removeAttribute("onclick");
+}
+
+function addNewItem() {
+
+    let form = document.getElementById("addItem-form");
+    let items = document.getElementById("items");
+    let desc = document.getElementById("desc-change");
+    items.innerHTML =
+        "   <div class=\"item\">" +
+        "       <span class=\"item-name\"><strong>"+form.name.value+"</strong></span>\n" +
+        "       <span class=\"item-desc\"><p><strong>Description:</strong> "+desc.value+"</p></span>\n" +
+        "       <span class=\"item-price\">$"+ form.price.value +"</span>\n" +
+        "       <span class=\"item-quantity\">"+ form.quant.value +"</span>\n" +
+        "       <span class=\"item-edit-icon\"><p>!</p></span>\n" +
+        "       <span class=\"item-hide-icon\"><p>hide</p></span>" +
+        "   </div>"
+        + items.innerHTML;
+
+    resetAddItem()
+}
+
+function resetAddItem(){
+
+    let addform = document.getElementById("addItem-form");
+    addform.reset();
+    addform.setAttribute("hidden", "hidden");
+
+    let button = document.getElementById("addItem");
+    button.setAttribute("onclick", "addItem()")
+}
+
+function editItem(item){
+
+    let inputs = item.getElementsByTagName("span");
+    alert(inputs[0])
+
+    item.innerHTML =
+        "<form id='addItem-form' onsubmit='changeItemDetails(this);return false'>" +
+        "    <span class=\"item-name\"><input type='text' placeholder='Enter item title' required>"+inputs[0]+"</span>\n" +
+        "    <span class=\"item-desc\"><textarea name='change-description' placeholder='Enter Description' cols='40' rows='6' required></textarea></span>\n" +
+        "    <span class=\"item-price\">$<input type='text' required></span>\n" +
+        "    <span class=\"item-quantity\"><input type='text' required></span>\n" +
+        "    <span id='confirm-change' class=\"item-edit-icon\"><button type=\"submit\">&#10003</button></span>\n" +
+        "    <span id='delete-change' class=\"item-delete-icon\" onclick=\"resetAddItem()\">X</span>" +
+        "</form>"
+
+
+    let addItemButton = document.getElementById("addItem");
+    addItemButton.removeAttribute("onclick");
+
+    let editButtons = document.getElementsByClassName('item-edit-icon');
+    for(var i=0;i<editButtons.length;i++){
+        editButtons[i].removeAttribute("onclick")
+    }
+}
+
+// function changeItemDetails(item){
+//
+//     // item
+//
+// }
