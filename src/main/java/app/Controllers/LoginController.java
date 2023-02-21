a0af40341b93dc31fcb91142dc59d2579716a52d
Jingyi Cui <jingyi.cui1@ucdconnect.ie>
2023-02-21 23:40:05 +0000

    the increment/decrement button on cart page works

--- src/main/java/app/Controllers/LoginController.java
+++ src/main/java/app/Controllers/LoginController.java
@@ -1,4 +1,28 @@
 package app.Controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.HashMap;
+
 public class LoginController {
+    private HashMap<String,String> accounts = new HashMap<>();
+    private int count=0;
+
+    @PostMapping("/login")
+    public void Registration(@ModelAttribute User user, HttpServletResponse response){
+        accounts.put(user.getEmail(),user.getPassword());
+    }
+
+    @PostMapping("/login")
+    public String Login(@ModelAttribute User use, HttpServletResponse response){
+        if(accounts.get(use.getEmail()) == null){
+            return "login.html";
+        }
+        else{
+            return "login.html";
+        }
+    }
+
 }
---
+++ src/main/java/app/User.java
@@ -0,0 +1,26 @@
+package app;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String email;
+    private String password;
+    public User(){}
+
+}
