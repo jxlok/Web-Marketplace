0aec8f06ebe132166e32d084403ec33ff0e25a90
Jingyi Cui <jingyi.cui1@ucdconnect.ie>
2023-03-05 21:50:06 +0000

    1.updated cart.js and cart.html to load data. 2. change the item price into 2 decimal form. 3.fixed controller conflict error

--- src/main/java/app/Controllers/CartController.java
+++ src/main/java/app/Controllers/CartController.java
@@ -14,24 +14,48 @@ import java.util.Optional;
 
 @Controller
 public class CartController {
+    final static double TAX_RATE = 0.1d;
     // depends on interface instead of implementation is beneficial:
     // 1. it doesn't depend on concrete implementation, so there is no need to change attribute type when underneath implementation changes.
     // 2. In tests, you can create dummy implementation of dependencies for testing purpose
     @Autowired
     CartService cartService;
 
-    @PostMapping("/cart")
+    @GetMapping("/cart")
     //collect card information and order information
-
     public String cartPage(Model model) {
         var cartItems = cartService.getCart(111);
+        var totalItemCount = cartItems.stream().map(ci -> ci.getCartItem().getQuantity()).reduce(0, Integer::sum);
+        var cartPretaxTotal = cartItems.stream().map(ci -> ci.getCartItem().getQuantity() * ci.getItem().getPrice()).reduce(0d, Double::sum);
+        var cartTax = TAX_RATE * cartPretaxTotal;
+        var cartTaxedTotal = cartTax + cartPretaxTotal;
+
         model.addAttribute("cartDisplayItems", cartItems);
-        model.addAttribute(
-                "cartItemsCount",
-                cartItems.stream().map(ci -> ci.getCartItem().getQuantity()).reduce(0, Integer::sum)
-        );
+        model.addAttribute("cartItemsCount", totalItemCount);
+        model.addAttribute("cartPretaxTotal", cartPretaxTotal);
+        model.addAttribute("cartTax", cartTax);
+        model.addAttribute("cartTaxedTotal", cartTaxedTotal);
+        model.addAttribute("taxRate", TAX_RATE);
+        model.addAttribute("taxRateInPercentage", TAX_RATE * 100);
 
         return "cart.html";
     }
 
+    @PatchMapping("/cart/items/{cartItemId}")
+    public ResponseEntity<Void> setCartItemQuantity(
+            @PathVariable(name = "cartItemId") int cartItemId,
+            @RequestBody CartItemQuantityUpdateRequest request) {
+
+        boolean succeeded = cartService.updateCartItemQuantity(111, cartItemId, request.getQuantity());
+
+        return succeeded ? ResponseEntity.ok().build() : ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/cart/items/{cartItemId}")
+    public ResponseEntity<Void> removeCartItem(
+            @PathVariable(name = "cartItemId") int cartItemId) {
+        var succeeded = cartService.deleteCartItem(111, cartItemId);
+
+        return succeeded ? ResponseEntity.ok().build() : ResponseEntity.badRequest().build();
+    }
 }
--- src/main/resources/static/admin-script.js
+++ src/main/resources/static/admin-script.js
@@ -1,176 +1,80 @@
-window.onload = function(){
-    activateEditButtons()
+function increment(container, displayCartItem, taxRate) {
+    let counter = container.getElementsByClassName("counting")[0];
+    let totalCounter = document.getElementById("total-items-count");
 
+    $.ajax({
+        contentType: "application/json",
+        data: JSON.stringify({ "quantity": parseInt(counter.innerText) + 1 }),
+        dataType: "text", // because response is not json type, so ajax will try to parse it to json and fail
+        method: "PATCH",
+        url: `/cart/items/${displayCartItem.cartItem.id}`,
+        success: function() {
+            counter.innerText = parseInt(counter.innerText) + 1;
+            updateTotalItemCount(1);
+            updateOrderSummary(parseFloat(displayCartItem.item.price), taxRate);
+        },
+        error: function(resp, error) {
+            console.log(`operation failed, status code: ${resp.status}, error: ${error}`);
+        }
+    });
 }
 
+function decrement(container, displayCartItem, taxRate) {
+    let counter = container.getElementsByClassName("counting")[0];
+    let totalCounter = document.getElementById("total-items-count");
 
-let Editlistener = function editMode(){
-    editItem(this.parentNode);
-}
-
-function activateEditButtons(){
-    let items = document.getElementsByClassName("item-edit-icon")
-    for(var i=0;i<items.length;i++){
-        items[i].addEventListener("click", Editlistener);
+    // we don't allow customer to reduce quantity of any cart item down to zero, so 1 is minimum quantity a customer can buy
+    if (parseInt(counter.innerText) > 1) {
+        $.ajax({
+            contentType: "application/json",
+            data: JSON.stringify({ "quantity": parseInt(counter.innerText) - 1 }),
+            dataType: "text", // because response is not json type, so ajax will try to parse it to json and fail
+            method: "PATCH",
+            url: `/cart/items/${displayCartItem.cartItem.id}`,
+            success: function() {
+                counter.innerText = parseInt(counter.innerText) - 1;
+                updateTotalItemCount(-1);
+                updateOrderSummary(parseFloat(displayCartItem.item.price) * -1, taxRate);
+            },
+            error: function(resp, error) {
+                console.log(`operation failed, status code: ${resp.status}, error: ${error}`);
+            }
+        });
     }
 }
 
-function deactivateEditButtons(){
-    let items = document.getElementsByClassName("item-edit-icon")
-    for(var i=0;i<items.length;i++){
-        items[i].removeEventListener("click", Editlistener);
-    }
+function removeItem(displayCartItem, taxRate){
+    let item = document.getElementById(`cart-item-${displayCartItem.cartItem.id}`);
+    let counter = item.getElementsByClassName("counting")[0];
+    let totalCounter = document.getElementById("total-items-count");
+
+    $.ajax({
+        method: "DELETE",
+        url: `/cart/items/${displayCartItem.cartItem.id}`,
+        error: function(resp, error) {
+            console.log(`operation failed, status code: ${resp.status}, error: ${error}`);
+        }
+    });
+    item.remove();
+    updateTotalItemCount(parseInt(counter.innerText) * -1);
+    updateOrderSummary(parseInt(counter.innerText) * parseFloat(displayCartItem.item.price) * -1, taxRate);
 }
 
-function activateHideButtons(){
-    let items = document.getElementsByClassName("item-hide-icon")
-    for(var i=0;i<items.length;i++){
-        items[i].setAttribute('aria-disabled', "true")
-    }
+function updateTotalItemCount(itemChange) {
+    let totalCounter = document.getElementById("total-items-count");
+    totalCounter.innerText = String(parseInt(totalCounter.innerText) + parseInt(itemChange));
 }
+function updateOrderSummary(pretaxPriceChange, taxRate) {
+    let pretaxSpan = document.getElementById("order-summary-pretax-total");
+    let pretax = parseFloat(pretaxSpan.innerText);
+    let newPretax = pretax + parseFloat(pretaxPriceChange);
+    pretaxSpan.innerText = newPretax.toFixed(2);
 
-function deactivateHideButtons(){
-    let items = document.getElementsByClassName("item-hide-icon")
-    for(var i=0;i<items.length;i++){
-        items[i].removeAttribute('aria-disabled')
-    }
-}
+    let taxSpan = document.getElementById("order-summary-tax");
+    let newTax = newPretax * parseFloat(taxRate);
+    taxSpan.innerText = newTax.toFixed(2);
 
-// function activateUnhideButton(){
-//     let items = document.getElementsByClassName("item-unhide-icon")
-//     for(var i=0;i<items.length;i++){
-//         items[i].removeEventListener("click", HideListener);
-//     }
-// }
-//
-// function analytics_tab(){
-//     let analytics = document.getElementById('center-info');
-//     let items = document.getElementById('sales-section');
-//
-//     analytics.removeAttribute("hidden");
-//     items.setAttribute("hidden", "hidden");
-// }
-//
-// function items_tab(){
-//     let analytics = document.getElementById('center-info');
-//     let items = document.getElementById('sales-section');
-//
-//     items.removeAttribute("hidden");
-//     analytics.setAttribute("hidden", "hidden");
-// }
-//
-// function displayItemForSale(){
-//     let saleSection = document.getElementById('items-for-sale');
-//     let hiddenSection = document.getElementById('hidden-items-container');
-//
-//     saleSection.removeAttribute("hidden");
-//     hiddenSection.setAttribute("hidden", "hidden")
-//
-//     let saleTab = document.getElementById('sale-tab')
-//     let hiddenTab = document.getElementById('hidden-tab')
-//
-//     saleTab.style.borderBottom = 0
-//     hiddenTab.style.borderBottom = "1px solid silver"
-// }
-//
-// function displayHiddenTab(){
-//     let saleSection = document.getElementById('items-for-sale');
-//     let hiddenSection = document.getElementById('hidden-items-container');
-//
-//     hiddenSection.removeAttribute("hidden");
-//     saleSection.setAttribute("hidden", "hidden")
-//
-//     let saleTab = document.getElementById('sale-tab')
-//     let hiddenTab = document.getElementById('hidden-tab')
-//
-//     hiddenTab.style.borderBottom = 0
-//     saleTab.style.borderBottom = "1px solid silver"
-//
-// }
-
-function addItem(){
-    let form = document.getElementById("addItem-form");
-    form.removeAttribute("hidden")
-
-    let addItemButton = document.getElementById("addItem");
-    addItemButton.removeAttribute("onclick");
-
-    deactivateEditButtons()
-}
-//
-// function addNewItem() {
-//
-//     let form = document.getElementById("addItem-form");
-//     let items = document.getElementById("items");
-//     let desc = document.getElementById("desc-new");
-//     items.innerHTML =
-//         "   <div class=\"item\">" +
-//         "       <span class=\"item-name\"><strong>"+form.name.value+"</strong></span>\n" +
-//         "       <span class=\"item-desc\"><p><strong>Description:</strong> "+desc.value+"</p></span>\n" +
-//         "       <span class=\"item-price\">$"+ form.price.value +"</span>\n" +
-//         "       <span class=\"item-quantity\">"+ form.quantity.value +"</span>\n" +
-//         "       <span class=\"item-edit-icon\"><p>edit</p></span>\n" +
-//         "       <span class=\"item-hide-icon\"><p>hide</p></span>" +
-//         "   </div>"
-//         + items.innerHTML;
-//
-//     resetAddItem()
-//
-// }
-
-function resetAddItem(){
-
-    let addform = document.getElementById("addItem-form");
-    addform.reset();
-    addform.setAttribute("hidden", "hidden");
-
-    activateAddButton()
-    activateEditButtons()
-    activateHideButtons();
-
-}
-
-function activateAddButton(){
-    let button = document.getElementById("addItem");
-    button.setAttribute("onclick", "addItem()")
-}
-
-
-function editItem(item){
-
-    item.setAttribute("id", "editing")
-    let inputs = item.getElementsByTagName("span");
-
-    let name = inputs[0].innerText;
-    let desc = inputs[1].innerText.slice(13,);
-    let price = inputs[2].innerText.slice(1,);
-    let quantity = parseInt(inputs[3].innerText.slice(1,));
-
-    item.innerHTML =
-        "<form id='editItem-form' method=\"POST\"'>" +
-        "    <span class=\"item-name\"><input type='text' name='itemName' placeholder='Enter item title' value='"+name+"' readonly></span>\n" +
-        "    <span class=\"item-desc\"><textarea id=\"desc-changing\" name=\"description\" placeholder='Enter Description' cols='40' rows='6' required>"+desc+"</textarea></span>\n"+
-        "    <span class=\"item-price\">$<input type='number' step='any'  name='price' value='"+price+"' required></span>\n" +
-        "    <span class=\"item-quantity\"><input type='number' step='1' name='stock' value='"+quantity+"' required></span>\n" +
-        "    <span id='confirm-change' class=\"item-change-icon\"><button type=\"submit\">&#10003</button></span>\n" +
-        "    <span id='delete-change' class=\"item-delete-icon\"><a onclick='location.reload()'>cancel</span>\n" +
-        "</form>"
-
-    let addItemButton = document.getElementById("addItem");
-    addItemButton.removeAttribute("onclick");
-
-    deactivateEditButtons()
-    deactivateHideButtons()
-}
-
-function show_status_list(node){
-
-    let parent = node.parentNode
-    let div = parent.getElementsByClassName("status-list")[0];
-    if(div.classList.contains("show")){
-        div.classList.remove("show")
-    }
-    else{
-        div.classList.add("show")
-    }
+    let taxedTotalSpan = document.getElementById("order-summary-taxed-total");
+    let newTaxedTotal = newPretax + newTax;
+    taxedTotalSpan.innerText = newTaxedTotal.toFixed(2);
 }
