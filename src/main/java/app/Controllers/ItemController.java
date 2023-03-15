8d86fc692aeb8f1bb941685e939e1afea81870d2
Haocheng12 <haocheng.sun123@gmail.com>
2023-03-15 22:34:44 +0000

    Fixed bugs and add new feature to catalogue and  item page
    1.Add new footer
    2.Change the color of the button
    check if the item has another version and correctly generate the button
    3.If the item is out of stock, change the add to cart button to out of stock
    4.compress the video resources

--- src/main/java/app/Controllers/ItemController.java
+++ src/main/java/app/Controllers/ItemController.java
@@ -1,27 +1,91 @@
 package app.Controllers;
 
 import app.Entities.Item;
+import app.Service.CartService;
 import app.Service.ItemService;
+import app.SessionVariables;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.LinkedList;
+import java.util.List;
 
 
 @Controller
 public class ItemController {
 
     @Autowired
+    private SessionVariables sessionVariables;
+
+    @Autowired
     ItemService itemService;
+    @Autowired
+    CartService cartService;
 
-
+    List<Item> searchedItems = new LinkedList<>();
     @GetMapping("/item")
     public String item(@RequestParam("id") int id, Model model) {
+
+        if(sessionVariables.isSearching()){
+            model.addAttribute("myItems", searchedItems);
+        }
+        else {
+            model.addAttribute("myItems", itemService.getUnhiddenItems());
+        }
+
+        model.addAttribute("customerLoggedIn", sessionVariables.isCustomerLoggedIn());
+        model.addAttribute("adminLoggedIn", sessionVariables.isAdminLoggedIn());
+        model.addAttribute("searching", sessionVariables.isSearching());
+        var cartItems = cartService.getCart(111);
+        model.addAttribute("basketCount", cartItems.stream().map(ci -> ci.getCartItem().getQuantity()).reduce(0, Integer::sum));        sessionVariables.setSearching(false);
+
+
+
+
         Item item = itemService.getItem(id);
+
+        if(item.getStock()==0){
+            model.addAttribute("stock",0);
+        }else{
+            model.addAttribute("stock",item.getStock());
+        }
+
+        String itemName = item.getItemName();
+        boolean anotherVersion = false;
+        int itemIsTrained = item.getIsTrained();
+        for (Item otherItem : itemService.getUnhiddenItems()) {
+            if (otherItem.getItemName().equals(itemName) && otherItem.getIsTrained() != itemIsTrained) {
+                anotherVersion=true;
+
+                model.addAttribute("anotherID", otherItem.getItemId());
+            }
+        }
+        model.addAttribute("anotherVersion", anotherVersion);
+
+        if(item.getIsTrained()==1){
+            String buttonStatus = "Trained";
+            model.addAttribute("buttonStatus", buttonStatus);
+
+
+
+        }else {
+            String buttonStatus = "unTrained";
+            model.addAttribute("buttonStatus", buttonStatus);
+        }
 
         model.addAttribute("item", item);
         return "item.html";
+    }
+
+    @PostMapping("/item")
+    public String search(@RequestParam String search_query){
+        sessionVariables.setSearching(true);
+        searchedItems = itemService.getSearchedItems(search_query);
+        return "redirect:/item";
     }
 
 
